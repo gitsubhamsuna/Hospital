6a601dc6c4a68b725c3eec2dcf5badc22b498991
gitsubhamsuna <subhamsunagit@gmail.com>
2024-01-29 12:57:14 +0530

    add

--- src/test/java/P1.java
+++ src/test/java/P1.java
@@ -1,9 +1,8 @@
+import org.testng.annotations.Test;
 
 public class P1 {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	@Test
+	public void test() {
+		throw new NullPointerException("Java");
 	}
-
 }
